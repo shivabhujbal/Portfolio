d6518b0ac7311419e30bc7bcf860043ce9a97326
shivabhujbal <shivsambh2001bhujbal@gmail.com>
2024-12-04 18:58:30 +0530

    speed insights enabled

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+inject();
+injectSpeedInsights();
 
 
 createRoot(document.getElementById('root')).render(
@@ -10,5 +14,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-
-inject();
